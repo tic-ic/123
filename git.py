6efd1ef2d9ca2d565a3da052747bb3b0eb375828
nonu <din2in2@yahoo.com>
2021-07-18 13:21:59 +0300

    second

--- git.py
+++ git.py
@@ -1,1 +1,13 @@
 print('test git1);
+print("""
+configurarea userului asociat tie:
+
+git config --global user.mail "din2in2@yahoo.com"
+
+""");
+
+print("""
+salavarea unei modificari IN REPOZITUL LOCAL.
+
+git commit -m "first commit" 
+""");
